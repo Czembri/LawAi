73d684bc39f8640717aba2e928bfe3d51709b2ad
aleksandra.czembrowska <aleksandra.czembrowska.ext@geodis.com>
2023-10-24 23:46:32 +0200

    Feature: bot needs to remember context

--- API/api.py
+++ API/api.py
@@ -4,10 +4,31 @@ from AI.constitution import ConstitutionAI
 
 routes = Blueprint('routes', __name__)
 
-@app.route("/api/v1/constitution-ai", methods=["GET", "POST"])
-def constitutionAI():
-    data = request.get_json()
-    ai = ConstitutionAI(data["query"])
+
+@app.route("/api/v1/constitution-ai/load-data", methods=["GET"])
+def constitution_AI_data():
+    global ai_instance
+    ai_instance = _get_data()
     return jsonify({
-        'response': ai.getResponse()
+        'message': 'Data loaded successfully'
     })
+
+@app.route("/api/v1/constitution-ai", methods=["POST"])
+def constitution_AI():
+    data = request.get_json()
+    data_query = data["query"]
+    return jsonify({
+        'response': ai_instance.get_response(data_query)
+    })
+
+@app.route("/api/v1/constitution-ai/clear-memory", methods=["GET"])
+def clear():
+    ai_instance.clear_memory()
+    return jsonify({
+        'response': 'Memory cleared'
+    })
+
+def _get_data():
+    ai = ConstitutionAI()
+    ai.load_data()
+    return ai
--- run.py
+++ run.py
@@ -2,10 +2,12 @@ from API import app
 from API.api import routes
 from _config import constants
 import os
+from langchain.document_loaders import DirectoryLoader
+
+global data
 
 app.register_blueprint(routes, url_prefix="")
 
 if __name__ == "__main__":
-    
     os.environ["OPENAI_API_KEY"] = constants.API_KEY    
     app.run(debug=True, port=8001)
