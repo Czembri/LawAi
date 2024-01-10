b590067f764723ce76f6997a63b1aea61c4c9e03
aleksandra.czembrowska <aleksandra.czembrowska.ext@geodis.com>
2024-01-10 14:02:51 +0100

    feature: change of the context

--- AI/document_generator.py
+++ AI/document_generator.py
@@ -7,9 +7,14 @@ class DocumentGenerator(LawAIAbstract):
                 Jesteś polskim prawnikiem. Twoim zadaniem jest przeanalizować potrzeby klienta i na podstawie dostarczonych danych
                 wygenerować dokument zgodny z przyjętymi standardami dla danego typu dokumentu.
                 Dokument musi być tak sformatowany aby dało się go przekonwertować do formatu pdf.
+                Podpis klienta musi zostać umieszczony w odpowiednim miejscu tj.: po prawej stronie na dole kartki.
+                Data ma znajdować się w prawym górnym rogu dokumentu. Dane klienta muszą być umieszczone w lewym górnym rogu dokumentu,
+                a dane adresowe w lewym dolnym rogu dokumentu nad treścią.
+                Tytuł dokumentu musi znajdować się na środku, nad treścią dokumentu a pod adresatem. Ma być pogrubiony i powinien rozpoczynać się dużą literą.
                 Dokument musi zawierać dane przesłane przez klienta. "Zawartość dokumentu" powinna zostać dostosowana do typu dokumentu i napisana
                 w języku urzędowym, co za tym idzie możesz modyfikować to pole. Nie dodawaj zbędnych komentarzy do dokumentu. Potrzebuję żebyś 
                 wygenerował mi czysty plik. Zadbaj o poprawność interpunkcji i ortografii oraz o formatowanie tekstu. Dokument musi zawierać odpowiednie nagłówki i stopki.
+                Jeżeli klient nie podał daty, lub nie została wysłana wypełnij to pole datą dzisiejszą.
         """}]
 
         super().__init__(self.messages)
